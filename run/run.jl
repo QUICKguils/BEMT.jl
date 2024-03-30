3d6e97c7fd11a123acffb474b5efc64d0775f6cd
QUICKguils <guilainsjc@gmail.com>
2024-03-30 14:11:46 +0100

    Track main package with ]dev instead of ]add

--- run/run.jl
+++ run/run.jl
@@ -1,11 +1,3 @@
-# Push the local run environment to the environment stack,
-# so that this script can use the extra packages of the run environment.
-#
-# This is the way to do, until
-# https://github.com/JuliaLang/Pkg.jl/issues/1233
-# will be implemented.
-# push!(LOAD_PATH, @__DIR__)
-
 using ArgParse
 
 using BEMT
