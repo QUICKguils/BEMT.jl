60a782c1bf2d34be51480fd22ff53f31e27c4439
QUICKguils <guilainsjc@gmail.com>
2024-05-06 11:19:13 +0200

    Split Part 2 code in smaller subfunctions

--- run/Part2.jl
+++ run/Part2.jl
@@ -7,7 +7,7 @@ Compare your results with the experimental results.
 """
 module Part2
 
-using GLMakie  # TODO: find a way to conditionnaly load GLMakie
+using GLMakie  # TODO: find a way to conditionally load GLMakie
 using BEMT
 using ..Statement: Statement as Stm
 
@@ -21,15 +21,29 @@ const μ            = 17.89e-6                                             # Dyn
 
 """Execute the second part of the project."""
 function main(args)
+    thrusts, powers, Ωs, v∞s = reproduce_report(args)
+    J, C_T, C_P, eta = performance_coefficients(thrusts, powers, Ωs, v∞s)
+
+    # Plot the coefficients, if desired
+    args["plot"] && plot_sol(J, C_T, C_P, eta)
+
+    return J, C_T, C_P, eta
+end
+
+"""
+    reproduce_report(args)
+Reproduce the experimental method conducted in the reference NACA report.
+
+Calculate the propeller thrust and power for an increasing wind speed, up to a maximum of
+115 mph. Then decrease the propeller rotation speed to further increase the advance ratio.
+Stop the computation when the computed thrust becomes negative.
+"""
+function reproduce_report(args)
     thrusts = [Float64[] for _ ∈ eachindex(θ75_range)]
     powers  = [Float64[] for _ ∈ eachindex(θ75_range)]
     Ωs      = [Float64[] for _ ∈ eachindex(θ75_range)]
     v∞s     = [Float64[] for _ ∈ eachindex(θ75_range)]
 
-    # Reproduce the experimental method conducted in [1].
-    # Calculate the propeller thrust and power for an increasing wind speed, up to a maximum
-    # of 115 mph. Then decrease the propeller rotation speed to further increase the advance
-    # ratio. Stop the computation when the computed thrust becomes negative.
     for θ75_index ∈ eachindex(θ75_range)
         v∞_index = 1
         Ω = Ω_range[θ75_index]
@@ -39,7 +53,6 @@ function main(args)
             oper = OperatingConditions(Ω, θ75_range[θ75_index], v∞, ρ, μ)
             sol = bem(Stm.prop, oper, sdiv=args["sdiv"])
 
-            @show v∞_index  # TODO: remove when debugged
             sol.thrust <= 0 && break
 
             push!(Ωs[θ75_index],      oper.Ω)
@@ -56,23 +69,30 @@ function main(args)
         end
     end
 
+    return thrusts, powers, Ωs, v∞s
+end
+
+"""
+    performance_coefficients(thrusts, powers, Ωs, v∞s)
+Compute the performance coefficients associated to the experimental reproduction results.
+"""
+function performance_coefficients(thrusts, powers, Ωs, v∞s)
     J   = [Float64[] for _ ∈ eachindex(θ75_range)]  # Advance ratios
     C_T = [Float64[] for _ ∈ eachindex(θ75_range)]  # Thrust coefficients
     C_P = [Float64[] for _ ∈ eachindex(θ75_range)]  # Power coefficients
     eta = [Float64[] for _ ∈ eachindex(θ75_range)]  # Propulsive efficiencies
 
     for i ∈ eachindex(θ75_range)
-        J[i] = (v∞s[i] ./ (2*Stm.span*Ωs[i])
-            / C.mph2ms * C.ft2m * C.tr2rad)
-        C_T[i] = (4 * thrusts[i] ./ ((2*Stm.span)^4*ρ*Ωs[i].^2)
-            / C.lbf2n * C.ft2m^4 * C.slug2kg / C.ft2m^3 * C.tr2rad^2)
-        C_P[i] = (4 * powers[i] ./ ((2*Stm.span)^5*ρ*Ωs[i].^3)
-            / C.hp2w * C.ft2m^5 * C.slug2kg / C.ft2m^3 * C.tr2rad^3)
+        J[i]   = v∞s[i] ./ (2*Stm.span*Ωs[i])
+        C_T[i] = 4 * thrusts[i] ./ ((2*Stm.span)^4*ρ*Ωs[i].^2)
+        C_P[i] = 4 * powers[i] ./ ((2*Stm.span)^5*ρ*Ωs[i].^3)
         eta[i] = C_T[i] ./ C_P[i] .* J[i]
     end
 
-    # Plot the coefficients, if desired
-    args["plot"] && plot_sol(J, C_T, C_P, eta)
+    # NOTE: to translate in imperial units, use:
+    # J   = J   / C.mph2ms * C.ft2m * C.tr2rad
+    # C_T = C_T / C.lbf2n * C.ft2m^4 * C.slug2kg / C.ft2m^3 * C.tr2rad^2
+    # C_P = C_P / C.hp2w * C.ft2m^5 * C.slug2kg / C.ft2m^3 * C.tr2rad^3
 
     return J, C_T, C_P, eta
 end
@@ -95,11 +115,12 @@ function plot_sol(J, C_T, C_P, eta)
     end
 
     # Dress the plot: legend and layout
-    Legend(fig[1, 1:2], ax_eta, L"\theta_{75}",
-           titleposition=:left, titlegap=20, orientation=:horizontal)
+    Legend(
+        fig[1, 1:2], ax_eta, L"\theta_{75}",
+        titleposition=:left, titlegap=20, orientation=:horizontal
+    )
     rowsize!(fig.layout, 2, Auto(0.6))
 
-    # Display the plot
     display(fig)
 
     return nothing
